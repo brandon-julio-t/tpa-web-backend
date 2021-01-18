aa6e105682f4206d3ca1a4a50104aef34e77ba70
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2021-01-18 12:21:28 +0700

    drop database on every run

--- bootstrap/database_migration.go
+++ bootstrap/database_migration.go
@@ -7,6 +7,20 @@ import (
 )
 
 func init() {
+	if err := facades.UseDB().Migrator().DropTable(
+		&models.Country{},
+		&models.User{},
+		&models.RegisterVerificationToken{},
+		&models.Report{},
+		&models.UnsuspendRequest{},
+		&models.AssetFile{},
+		&models.Game{},
+		&models.GameSlideshow{},
+		&models.GameTag{},
+	); err != nil {
+		log.Fatal(err)
+	}
+
 	if err := facades.UseDB().AutoMigrate(
 		&models.Country{},
 		&models.User{},
